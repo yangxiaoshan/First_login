e8dc8c8441affad5d353ae592753853c0d1f9daf
yangxiaoshan <276497736@qq.com>
2017-11-13 21:10:27 +0800

    部门编辑

--- src/main/java/com/yxs/crm/post/web/action/PostAction.java
+++ src/main/java/com/yxs/crm/post/web/action/PostAction.java
@@ -2,17 +2,65 @@ package com.yxs.crm.post.web.action;
 
 import com.yxs.crm.base.BaseAction;
 import com.yxs.crm.post.service.PostService;
+import com.yxs.crm.staff.domain.Department;
 import com.yxs.crm.staff.domain.Post;
+import java.util.List;
 
 /**
  * Created by dllo on 17/11/11.
  */
-public class PostAction extends BaseAction<Post,PostService> {
-    public String addPost(){
+public class PostAction extends BaseAction<Post, PostService> {
+    private List<Post> posts;
+    private List<Department> departments;
+    private int deptID;
+
+    public String addPost() {
+
+        Department d = new Department();
+        d.setDeptID(deptID);
+
+        getModel().setDepartment(d);
+
         Post post = service.addPost(getModel());
-        if (post != null){
+        if (post != null) {
             return SUCCESS;
         }
         return INPUT;
     }
+
+    public String getPost() {
+
+        posts = service.getPost();
+        return SUCCESS;
+    }
+    public String getDeptList(){
+        departments=service.getDeptList();
+        return SUCCESS;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
+
+    public int getDeptID() {
+        return deptID;
+    }
+
+    public void setDeptID(int deptID) {
+        this.deptID = deptID;
+    }
 }
+
+
